2b14be751cf78ea134beb7ac7eeea06d9f6190eb
samir <sa.samir213002019@gmail.com>
2026-05-18 07:22:54 +0600

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,30 @@
+let person={
+
+
+    name:"samir",
+    age:23,
+
+    city:"dhaka"
+};
+
+console.log(person)
+
+console.log(person.name);
+
+let mark=7;
+
+if( mark>=80){
+    console.log("A+")
+}
+
+else if 
+      (mark<=80)
+{
+    console.log("A")
+
+
+}
+
+else {
+    console.log("f")
+}
